f999848e2686787e2c3c58ec8f6f30b152e6b208
Nikolay Prodanov <Nikolay.Prodanov@ibm.com>
2025-05-15 00:30:58 +0300

    Added public ssh key

--- hosts/nix-test-vm/configuration.nix
+++ hosts/nix-test-vm/configuration.nix
@@ -20,6 +20,9 @@
     home = "/home/vmtest";
     extraGroups = [ "wheel" ];
     hashedPassword = "$6$O8dxVaB.wkZD4VSy$PqSkgKQunLVqiWXXMk39EwKzZqHBAtQ0mlag.Haj3LqTCrITF2oJMLxnDw7YDkDd7D7Ztvc2Zg95ziX553xMn1";
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHciq66oygoT3sgZOx5dA7nNK9JgpTqSRiDqVrukz6wQ azuread\\nikolayprodanov@IBM-PW09W916"
+    ];
   };
 
   environment.systemPackages = with pkgs; [
