e30386fbc4f7af75f446e8f0b95acdbf7225b40e
Nikolay Prodanov <Nikolay.Prodanov@ibm.com>
2025-05-14 23:25:22 +0300

    Added disko configuration

---
+++ hosts/nix-test-vm/disko.nix
@@ -0,0 +1,36 @@
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        device = "/dev/sda";
+        type = "disk";
+        content = {
+          type = "table";
+          format = "msdos";
+          partitions = {
+            swap = {
+              start = "-4G";
+              end = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+            root = {
+              part-type = "primary";
+              bootable = true;
+              start = "1M";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
