f052eac9e730e74585750b0b2c9d5adc48220592
Nikolay Prodanov <Nikolay.Prodanov@ibm.com>
2025-05-15 15:15:27 +0300

    Add ZSH with plugins

--- home-config/users/vmtest/home.nix
+++ home-config/users/vmtest/home.nix
@@ -23,26 +23,31 @@
     zulu17
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   home.sessionVariables = {
     EDITOR = "vim";
   };
 
+  nixpkgs.overlays = [
+    (import ../../../overlays/viber.nix)
+  ];
+
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    zsh-autoenv.enable = true;
+    enableCompletions = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "robbyrussell";
+      plugins = [
+        git
+        history
+      ];
+    };
+  };
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
