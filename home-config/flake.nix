9a56fed838b6c2e8e670ceaefcb606ac78f2dc6c
Nikolay Prodanov <Nikolay.Prodanov@ibm.com>
2025-05-15 15:39:04 +0300

    Add ZSH with plugins

--- home-config/flake.nix
+++ home-config/flake.nix
@@ -10,14 +10,14 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { nixpkgs, home-manager, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       homeConfigurations."vmtest" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
+        inherit inputs;
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./users/vmtest/home.nix ];
