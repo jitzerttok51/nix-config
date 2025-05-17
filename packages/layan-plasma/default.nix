2aaf7fbc90b9926c08fa7082596237f73aef527d
Nikolay Prodanov <Nikolay.Prodanov@proton.me>
2025-05-18 00:06:11 +0300

    Added Layan plasma theme

--- packages/layan-plasma/default.nix
+++ packages/layan-plasma/default.nix
@@ -1,18 +1,21 @@
 # https://nix.dev/tutorials/packaging-existing-software.html
 # Use in flake pkgs.callPackage ./packages/layan-plasma {};
-{ stdenv, fetchFromGitHub }: stdenv.mkDerivation {
-  pname = "";
+{ stdenv, fetchFromGitHub, lib }: stdenv.mkDerivation {
+  pname = "layan-plasma";
   version = "2025-02-13";
 
   src = fetchFromGitHub {
     owner = "vinceliuice";
     repo = "Layan-kde";
     rev = "2025-02-13";
-    sha256 = "";
+    sha256 = "sha256-T69bGjfZeOsJLmOZKps9N2wMv5VKYeo1ipGEsLAS+Sg="; # Call first time to get hash
   };
 
   installPhase = ''
-    echo "$HOME"
+    mkdir -p $out/.local/share
+    export HOME=$out
+    unset name
+    bash -x install.sh
   '';
 
   meta = with lib; {
