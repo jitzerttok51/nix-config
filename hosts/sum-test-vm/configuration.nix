284b820a76233fc69281d76e36555a1f6783ac70
Nikolay Prodanov <Nikolay.Prodanov@ibm.com>
2025-05-19 14:41:18 +0300

    Added configuration for a test-vm

---
+++ hosts/sum-test-vm/configuration.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ...}: {
+
+    imports = [
+        ./hardware-configuration.nix
+        ../../modules/users/vmtest.nix
+    ];
+
+    networking.hostName = "nix-test-vm";
+
+    services.openssh = {
+        enable = true;
+        settings = {
+            PasswordAuthentication = true;
+        };
+    };
+
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [ "vmtest" ];
+
+    environment.systemPackages = with pkgs; [
+        vim
+        zip
+        unzip
+        git
+        home-manager
+    ];
+
+    services.xserver = {
+        enable = true;
+        libinput.enable = true;
+        displayManager.lightdm.enable = true;
+        desktopManager = {
+            cinnamon.enable = true;
+        };
+        displayManager.defaultSession = "cinnamon";
+    };
+}
